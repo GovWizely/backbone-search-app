e2f04fac30f49b2b6195c3100dd56d9ea6014a21
Victor Lim <victor@rrsoft.co>
2015-11-06 20:18:10 +0800

    redirect to index on route not found error

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 // enable bootstrap js component
-window.jQuery = require('jquery');
-require('../bower_components/bootstrap-sass/assets/javascripts/bootstrap.js');
+require('bootstrap');
 
 var React         = require('react');
 var ReactDOM      = require('react-dom');
 var useBasename   = require('history').useBasename;
 var createHistory = require('history').createHashHistory;
 var Router        = require('react-router').Router;
+var Route         = require('react-router').DefaultRoute;
 
 var IndexView  = require('./js/components/index-view');
 var ResultView = require('./js/components/result-view');
@@ -22,7 +22,8 @@ if (process.env.NODE_ENV === "production") {
 
 const routes = [
   { path: "/", component: IndexView },
-  { path: "/search", component: ResultView }
+  { path: "/search", component: ResultView },
+  { path: "*", component: IndexView },
 ];
 
 ReactDOM.render((

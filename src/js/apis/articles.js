182a9278a791f1ff20989442f9aa3853d9040151
Victor Lim <victor@rrsoft.co>
2016-02-17 18:07:58 +0800

    rename ITA

--- src/js/apis/articles.js
+++ src/js/apis/articles.js
@@ -4,9 +4,8 @@ module.exports = defineAPI('articles', {
   aggregations: {
     countries: { type: 'array' },
     industries: { type: 'tree' }
-//    trade_regions: { type: 'array' }
   },
-  displayName: 'International Trade Administration Market Intelligence',
+  displayName: 'Market Intelligence',
   endpoint: 'https://pluto.kerits.org/v1/articles/search',
   permittedParams: ['q', 'countries', 'industries', 'topics', 'trade_regions', 'offset'],
   maxOffset: 1000

b90e17d834a4abaf5b2c39b84b06417af72ec811
Victor Lim <victor@rrsoft.co>
2016-02-19 16:01:56 +0800

    modify article params if offset is negative or more than 999

--- src/js/apis/articles.js
+++ src/js/apis/articles.js
@@ -1,5 +1,12 @@
 import { defineAPI } from './utils.js';
 
+function transformParams(params) {
+  if (params.offset && (params.offset < 0 || params.offset > 999) ) {
+    params.offset = 0;
+  }
+  return params;
+}
+
 module.exports = defineAPI('articles', {
   aggregations: {
     countries: { type: 'array' },
@@ -8,5 +15,5 @@ module.exports = defineAPI('articles', {
   displayName: 'Market Intelligence',
   endpoint: 'https://pluto.kerits.org/v1/articles/search',
   permittedParams: ['q', 'countries', 'industries', 'topics', 'trade_regions', 'offset'],
-  maxOffset: 1000
+  transformParams
 });

34f4e132a929d3b809f28737b9a6e440355a0b6c
Victor Lim <victor@rrsoft.co>
2016-03-31 14:59:07 +0800

    add query action spec

--- src/js/actions/index.js
+++ src/js/actions/index.js
@@ -1,18 +1,9 @@
 import assign from 'object-assign';
-import * as apiActions from './api';
-import * as filterActions from './filter';
-import * as notificationActions from './notification';
-import * as pathActions from './path';
-import * as queryActions from './query';
-import * as queryExpansionActions from './query_expansion';
-import * as resultActions from './result';
-import * as windowActions from './window';
 
 module.exports = assign(
   {},
   require('./api'),
   require('./filter'),
-  require('./notification'),
   require('./path'),
   require('./query'),
   require('./query_expansion'),

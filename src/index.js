1578635670ebf6c108053a65e30230e9aacc4cad
Victor Lim <victor@rrsoft.co>
2016-04-01 10:15:18 +0800

    remove unused import

--- src/index.js
+++ src/index.js
@@ -8,15 +8,14 @@ require('es6-promise').polyfill();
 import React from 'react';
 import { render } from 'react-dom';
 import { createHashHistory as createHistory } from 'history';
-import { IndexRoute, Router, Route } from 'react-router';
+import { Router, Route } from 'react-router';
 import { Provider } from 'react-redux';
-import { syncReduxAndRouter, routeReducer } from 'redux-simple-router';
+import { syncReduxAndRouter } from 'redux-simple-router';
 
 import App from './js/containers/app';
 import Index from './js/containers/index';
 import Search from './js/containers/search';
 import configureStore from './js/store/configureStore';
-import apis from './js/apis';
 
 const history = createHistory({ });
 const store = configureStore();

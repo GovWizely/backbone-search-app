6b3cf3e2a6c6bf4496d488f36e10a0eea382b868
Victor Lim <victor@rrsoft.co>
2016-04-04 14:55:58 +0800

    eslint fix

--- src/js/store/configureStore.production.js
+++ src/js/store/configureStore.production.js
@@ -1,9 +1,6 @@
 import { compose, createStore, applyMiddleware } from 'redux';
-import createLogger from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
 import reducers from '../reducers';
-
-const loggerMiddleware = createLogger();
 
 const wrapperCreateStore = compose(
   applyMiddleware(

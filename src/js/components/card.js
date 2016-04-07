b4afeb396aed2d297abd720295061b44fba2b916
Victor Lim <victor@rrsoft.co>
2016-04-07 21:31:08 +0800

    remove extra function call when creating card item

--- src/js/components/card.js
+++ src/js/components/card.js
@@ -1,46 +1,29 @@
 import React, { PropTypes } from 'react';
 import ResultList from './result-list';
 
-const View = ({ displayedItems, isFetching, items, template }) => {
+const Card = ({ isFetching, displayedItems, items, label, onClick, template }) => {
   if (isFetching || !items.length) return <noscript />;
 
   return (
-    <div className="mi-card__content">
-      <ResultList
-        displayedItems={ displayedItems }
-        items={ items }
-        template={ template }
-      />
-    </div>
+    <section className="mi-card">
+      <header className="mi-card__header" title={ label }>{ label }</header>
+
+      <div className="mi-card__box">
+        <div className="mi-card__content">
+          <ResultList
+            displayedItems={ displayedItems }
+            items={ items }
+            template={ template }
+          />
+        </div>
+
+        <footer className="mi-card__footer">
+          <a onClick={ onClick }>See More { label }</a>
+        </footer>
+      </div>
+    </section>
   );
 };
-View.propTypes = {
-  displayedItems: PropTypes.number,
-  isFetching: PropTypes.bool,
-  items: PropTypes.array.isRequired,
-  label: PropTypes.string,
-  template: PropTypes.func.isRequired
-};
-
-const Card = ({ isFetching, displayedItems, items, label, onClick, template }) => (
-  <section className="mi-card">
-    <header className="mi-card__header" title={ label }>{ label }</header>
-
-    <div className="mi-card__box">
-      <View
-        displayedItems={ displayedItems }
-        isFetching={ isFetching }
-        items={ items }
-        label={ label }
-        template={ template }
-      />
-
-      <footer className="mi-card__footer">
-        <a onClick={ onClick }>See More { label }</a>
-      </footer>
-    </div>
-  </section>
-);
 Card.propTypes = {
   displayedItems: PropTypes.number,
   isFetching: PropTypes.bool,

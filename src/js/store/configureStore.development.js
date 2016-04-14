86267fba47d012b487074f230f6567a87430264a
Victor Lim <victor@rrsoft.co>
2016-04-14 17:09:03 +0800

    update react to v15

--- src/js/store/configureStore.development.js
+++ src/js/store/configureStore.development.js
@@ -1,7 +1,6 @@
 import { compose, createStore, applyMiddleware } from 'redux';
 import createLogger from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
-import DevTools from '../containers/dev-tools';
 import reducers from '../reducers';
 
 const loggerMiddleware = createLogger();
@@ -10,8 +9,7 @@ const wrapperCreateStore = compose(
   applyMiddleware(
     thunkMiddleware,
     loggerMiddleware // Must be the last middleware to be included.
-  ),
-  DevTools.instrument()
+  )
 )(createStore);
 
 export default function configureStore(initialState) {

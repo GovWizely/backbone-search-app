6816a57416d4a76944bdcd0cd9e3bd05af269e6d
Victor Lim <victor@rrsoft.co>
2016-04-25 11:55:50 +0800

    update displayed url for trade leads and trade events

--- src/js/templates/trade_leads.js
+++ src/js/templates/trade_leads.js
@@ -3,16 +3,16 @@ import React from 'react';
 const href = (id) => `https://success.export.gov/tradelead?id=${id}`;
 
 export const trade_leads = {
-  ResultItem: ({ id, title, description, url }) => (
+  ResultItem: ({ id, title, description }) => (
       <article className="mi-trade-leads mi-result__item">
         <header>
           <a href={ href(id) } dangerouslySetInnerHTML={ { __html: title || description } }></a>
         </header>
-        <p><a href={ url }>{ url }</a></p>
+        <p><a href={ href(id) }>{ href(id) }</a></p>
         <p dangerouslySetInnerHTML={ { __html: description } }></p>
       </article>
   ),
-  CardItem: ({ id, title, description, url }) => (
+  CardItem: ({ id, title, description }) => (
     <article className="mi-trade-leads mi-card__item">
       <header>
         <a href={ href(id) } dangerouslySetInnerHTML={ { __html: title || description } }></a>

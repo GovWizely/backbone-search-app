e7ff4edb3c84eb8d9c4e375e5e26679245d029e4
Victor Lim <victor@rrsoft.co>
2016-04-25 13:53:23 +0800

    fix eslint errors

--- src/js/utils/action-helper.js
+++ src/js/utils/action-helper.js
@@ -1,4 +1,4 @@
-import { pick, reduce } from 'lodash';
+import { forEach, pick, reduce } from 'lodash';
 import assign from 'object-assign';
 import Url from 'url';
 import { parseAsTree } from '../utils/aggregation-parser';
@@ -38,9 +38,9 @@ export function formatMetadata(json, formats) {
 
 export function formatParams(query, permittedParams) {
   const params = pick(query, permittedParams);
-  for (const key in params) {
+  forEach(params, (value, key) => {
     if (Array.isArray(params[key])) params[key] = params[key].join(',');
-  }
+  });
   return params;
 }
 

dbd6707eff58bd3c70a96897edc055b9d12f6766
Victor Lim <victor@rrsoft.co>
2016-04-25 10:41:22 +0800

    use url from the response for ita_faqs

--- src/js/templates/ita_faqs.js
+++ src/js/templates/ita_faqs.js
@@ -3,18 +3,18 @@ import React from 'react';
 const href = (id) => `https://success.export.gov/faq?id=${id}`;
 
 export const ita_faqs = {
-  ResultItem: ({ id, answer, question }) => (
+  ResultItem: ({ id, answer, question, url }) => (
     <article className="mi-ita-faqs mi-result__item">
       <header>
-        <a href={ href(id) } dangerouslySetInnerHTML={ { __html: question } }></a>
+        <a href={ url } dangerouslySetInnerHTML={ { __html: question } }></a>
       </header>
       <p dangerouslySetInnerHTML={ { __html: answer } }></p>
     </article>
   ),
-  CardItem: ({ id, answer, question }, options) => (
+  CardItem: ({ id, answer, question, url }) => (
     <article className="mi-ita-faqs mi-card__item">
       <header>
-        <a href={ href(id) } dangerouslySetInnerHTML={ { __html: question } }></a>
+        <a href={ url } dangerouslySetInnerHTML={ { __html: question } }></a>
       </header>
       <p dangerouslySetInnerHTML={ { __html: answer } }></p>
     </article>

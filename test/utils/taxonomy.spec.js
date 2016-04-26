1b918c2dab850469a717079bd10eed93384f91a4
Victor Lim <victor@rrsoft.co>
2016-04-26 23:06:49 +0800

    taxonomy utils should return result in the same order as the input

--- test/utils/taxonomy.spec.js
+++ test/utils/taxonomy.spec.js
@@ -1,12 +1,18 @@
 import { expect } from 'chai';
-import { countryToAbbr } from '../../src/js/utils/taxonomy';
+import { abbrToCountry, countryToAbbr } from '../../src/js/utils/taxonomy';
 
 describe('taxonomy', () => {
-  const names = 'Zambia,Zimbabwe,United States,United Kingdom';
-
-  describe('#country', () => {
+  describe('#countryToAbbr', () => {
+    const names = 'Zambia,Zimbabwe,United States,United Kingdom';
     it('return abbreviations of countries', () => {
-      expect(countryToAbbr(names)).to.eql('GB,US,ZM,ZW');
+      expect(countryToAbbr(names)).to.eql('ZM,ZW,US,GB');
+    });
+  });
+
+  describe('#abbrToCountry', () => {
+    const abbrs = 'ZM,ZW,US,GB';
+    it('return fullname of countries', () => {
+      expect(abbrToCountry(abbrs)).to.eql('Zambia,Zimbabwe,United States,United Kingdom');
     });
   });
 });
